3c7907bbc6ac2ef555cf6b3f4a940fbdf30e4cf5
João Marcos <105825215+majinaru@users.noreply.github.com>
2022-12-07 11:56:59 -0400

    Atividade final da matéria

---
+++ Fundamentos-de-dev-de-software-2/atividadefinal1.1/src/Diretor.java
@@ -0,0 +1,36 @@
+public class Diretor extends Empregado{
+
+    private float salarioFixo;
+    private float gratificacoes;
+
+    public Diretor(String nome, String setor,
+                   float salariofixo, float gratificacoes){
+        super(nome, setor);
+        this.salarioFixo= salariofixo;
+        this.gratificacoes= gratificacoes;
+    }
+
+    public float getSalarioFixo() {
+        return salarioFixo;
+    }
+
+    public void setSalarioFixo(float salarioFixo) {
+        this.salarioFixo = salarioFixo;
+    }
+
+    public float getGratificacoes() {
+        return gratificacoes;
+    }
+
+    public void setGratificacoes(float gratificacoes) {
+        this.gratificacoes = gratificacoes;
+    }
+    public double getPagamento(){ return (salarioFixo+gratificacoes);}
+
+    @Override
+    public String toString() {
+        return"Diretor " + super.toString()+ "[Salario fixo= " +
+                salarioFixo + ", gratificacoes= " + gratificacoes + " Pagamento= "
+                + getPagamento() + "]";
+    }
+}
